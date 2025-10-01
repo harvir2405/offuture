c513f82d86910c6299e5d85493eb4dd1d865e55e
harvir2405 <harvirsingh2405@gmail.com>
2025-10-01 14:21:33 +0100

    Updated the slide by slide sql
    added to the slide by slide sql doc, should be everything
    
    might also have some tableau changes, not really sure what happened there, but also added Dan's tableau file

--- slide_by_slide_sql.sql
+++ slide_by_slide_sql.sql
@@ -1,12 +1,101 @@
---- creating the master view
+--- Creating the views
+-- First view 
+CREATE VIEW all_2509.team9 AS (
+	SELECT *
+	FROM offuture.order) 
+	
+SELECT * 
+FROM all_2509.team9;
+
+GRANT ALL ON all_2509.team9 TO de14_daki; 
+GRANT ALL ON all_2509.team9 TO de14_saan;
+GRANT ALL ON all_2509.team9 TO da14_haku; 
+
+
+-- Customer ID view
+CREATE VIEW all_2509.customer_id_team9 AS (
+SELECT order_id,
+       c.customer_id_long AS new_customer_id
+FROM offuture.order o
+JOIN offuture.customer c
+  ON o.customer_id = c.customer_id_short
+UNION
+SELECT o.order_id,
+       c.customer_id_long AS new_customer_id
+FROM offuture.order o
+JOIN offuture.customer c
+  ON o.customer_id = c.customer_id_long 
+  )
+  
+SELECT * 
+FROM all_2509.customer_id_team9
+ORDER BY new_customer_id; 
+
+GRANT ALL ON all_2509.customer_id_team9 TO de14_daki; 
+GRANT ALL ON all_2509.customer_id_team9 TO de14_saan;
+GRANT ALL ON all_2509.customer_id_team9 TO da14_haku; 
+
+-- Final master view
+CREATE VIEW all_2509.team9_master AS(
+	SELECT * 
+	FROM all_2509.team9
+	INNER JOIN offuture.order_item
+		USING(order_id)
+	INNER JOIN offuture.address
+		USING(address_id)
+	INNER JOIN offuture.product
+		USING(product_id)
+	INNER JOIN all_2509.customer_id_team9
+		USING(order_id)
+	INNER JOIN offuture.customer
+		ON customer.customer_id_long = customer_id_team9.new_customer_id
+);
+
+GRANT ALL ON all_2509.team9_master TO de14_daki; 
+GRANT ALL ON all_2509.team9_master TO de14_saan;
+GRANT ALL ON all_2509.team9_master TO da14_haku; 
+
+SELECT * 
+FROM all_2509.team9_master;
 
 
 
 ---slide 2 
 
+-- This code calculates the total sales including shipping cost
+SELECT
+	SUM(sales + shipping_cost) AS total_sales_and_ship
+FROM offuture.order_item;
+-- Output= 13995327.94
+
+-- This code shows a table for the total sales per year
+SELECT 
+	SUM(sales), 
+	DATE_TRUNC('year', order_date::date)
+FROM all_2509.team9_master
+GROUP BY DATE_TRUNC('year', order_date::date)
+ORDER BY DATE_TRUNC('year', order_date::date); 
+
 
 
 ---slide 3
+
+-- This code shows a table for the total profit per year
+SELECT 
+	SUM(profit), 
+	DATE_TRUNC('year', order_date::date)
+FROM all_2509.team9_master
+GROUP BY DATE_TRUNC('year', order_date::date)
+ORDER BY DATE_TRUNC('year', order_date::date);
+
+-- This code identifies the total profit for each country, listing the top 5
+SELECT 	
+	country AS "Country",
+	ROUND(SUM(profit), 0) AS "Total profit"
+FROM all_2509.team9_master
+GROUP BY country
+ORDER BY "Total profit" DESC
+LIMIT 5;
 
 
 
@@ -35,15 +124,35 @@ ORDER BY sub_category ASC;
 
 ---slide 5 
 
-
-
+-- This code shows the top 15 products, on average profit
+SELECT
+	AVG(profit),
+	product_id,
+	product_name
+FROM all_2509.team9_master
+GROUP BY
+	product_id,
+	product_name
+ORDER BY
+	AVG(profit) DESC
+LIMIT 15;
 
 
 
 ---slide 6 
 
-
-
+-- THis code shows the worst 15 products, on average profit
+SELECT
+	AVG(profit),
+	product_id,
+	product_name
+FROM all_2509.team9_master
+GROUP BY
+	product_id,
+	product_name
+ORDER BY
+	AVG(profit)
+LIMIT 15;
 
 
 
